acd20ae133b3f3b232c9667128b54c6264c9cbb0
Ben Mason <ben@wattmason.co.uk>
2021-08-20 22:53:02 +0100

    Remove deleted events

--- nft-indexer/src/resolvers/nft/index.ts
+++ nft-indexer/src/resolvers/nft/index.ts
@@ -1,18 +1,14 @@
 import BurnedToken from './BurnedToken';
-import BurnedTokenWithRemark from './BurnedTokenWithRemark';
 import ClaimedToken from './ClaimedToken';
 import CreatedClass from './CreatedClass';
-import DestroyedClass from './DestroyedClass';
 import MergedToken from './MergedToken';
 import MintedToken from './MintedToken';
 import TransferredToken from './TransferredToken';
 
 export default {
   BurnedToken,
-  BurnedTokenWithRemark,
   ClaimedToken,
   CreatedClass,
-  DestroyedClass,
   MergedToken,
   MintedToken,
   TransferredToken,
