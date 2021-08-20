164f8375d45ac4ce1a5525f5b667dd9d27bde3b4
Ben Mason <ben@wattmason.co.uk>
2021-08-20 13:48:03 +0100

    add classes to nft-models

---
+++ nft-models/src/classes.ts
@@ -0,0 +1,76 @@
+import { Schema, model } from 'mongoose';
+
+export interface IClass {
+  _id: number;
+  type: 'Claim' | 'Simple' | 'Merge';
+  owner: string;
+  totalIssuance: number;
+  startBlock?: number;
+  endBlock?: number;
+  properties: string; // TODO once bug is fixed
+}
+
+export interface ISimpleClass extends IClass {
+  quantity: number;
+}
+
+export interface IClaimClass extends IClass {
+  metadata: {
+    name: string;
+    description: string;
+    image: string;
+  };
+  metadataCID: string;
+  merkleProof: string;
+}
+
+export interface IMergeClass extends IClass {
+  todo: string;
+}
+
+const classSchema = new Schema<IClass>(
+  {
+    _id: Number,
+    type: { type: String, required: true, enum: ['Claim', 'Simple', 'Merge'] },
+    owner: { type: String, required: true },
+    totalIssuance: { type: Number, required: true },
+    startBlock: Number,
+    endBlock: Number,
+    properties: String, // TODO update once bug is fixed
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const options = {
+  discriminatorKey: 'type',
+};
+
+const simpleClassSchema = new Schema<ISimpleClass>(
+  {
+    quantity: { type: Number, required: true },
+  },
+  options
+);
+
+const claimClassSchema = new Schema<IClaimClass>(
+  {
+    metadata: { type: Object, required: true },
+    metadataCID: { type: String, required: true },
+    merkleProof: { type: String, required: true },
+  },
+  options
+);
+
+const mergeClassSchema = new Schema<IMergeClass>(
+  {
+    todo: String,
+  },
+  options
+);
+
+export const Class = model<IClass>('Class', classSchema);
+export const SimpleClass = Class.discriminator('Simple', simpleClassSchema);
+export const ClaimClass = Class.discriminator('Claim', claimClassSchema);
+export const MergeClass = Class.discriminator('Merge', mergeClassSchema);
