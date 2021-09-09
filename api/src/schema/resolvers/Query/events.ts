540ba8064e76fba7a1d75398b1355b1cbde6fc4a
StamfordDigital <git@stamford.digital>
2021-09-09 15:18:29 +0100

    Sorted events by createdAt desc

--- api/src/schema/resolvers/Query/events.ts
+++ api/src/schema/resolvers/Query/events.ts
@@ -1,6 +1,6 @@
 import { EventModel, Event } from 'nft-models';
 
 export default async function events(): Promise<Event[]> {
-  const events = await EventModel.find({});
+  const events = await EventModel.find().sort({ createdAt: 'desc' });
   return events;
 }
