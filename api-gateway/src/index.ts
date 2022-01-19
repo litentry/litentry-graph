9cf30918c36e7155807ba25394a71ff87971291e
Ben Mason <ben@wattmason.co.uk>
2022-01-19 16:51:27 +0000

    update remote schemas

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,24 +1,36 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { stitchSchemas } from '@graphql-tools/stitch';
-// import { introspectSchema } from '@graphql-tools/wrap';
+import { introspectSchema } from '@graphql-tools/wrap';
 import { schema } from 'substrate-api-proxy';
-// import makeRemoteExecutor from './makeRemoteExecutor';
+import makeRemoteExecutor from './makeRemoteExecutor';
 import config from './config';
 import { initSubstrateApi, SubstrateNetwork } from './substrateApi';
 
 async function makeGatewaySchema() {
-  // const subsquidExec = makeRemoteExecutor(
-  //   'https://app.gc.subsquid.io/beta/litentry/test/graphql'
-  // );
+  const subsquidExec = makeRemoteExecutor('http://localhost:4000/graphql');
+  const ethExec = makeRemoteExecutor(
+    'https://thegraph.com/hosted-service/subgraph/litentry/identity-subgraph'
+  );
+  const bscExec = makeRemoteExecutor(
+    'https://thegraph.com/hosted-service/subgraph/litentry/identity-subgraph-bsc'
+  );
 
   return stitchSchemas({
     subschemas: [
       schema,
-      // {
-      //   schema: await introspectSchema(subsquidExec),
-      //   executor: subsquidExec,
-      // },
+      {
+        schema: await introspectSchema(subsquidExec),
+        executor: ethExec,
+      },
+      {
+        schema: await introspectSchema(ethExec),
+        executor: ethExec,
+      },
+      {
+        schema: await introspectSchema(bscExec),
+        executor: bscExec,
+      },
     ],
   });
 }
