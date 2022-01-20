5acf8c5e78971f2a32558f83d746867f8559955d
Ben Mason <ben@wattmason.co.uk>
2022-01-20 14:42:19 +0000

    move remote schema URLs to env

--- api-gateway/src/config.ts
+++ api-gateway/src/config.ts
@@ -8,6 +8,13 @@ if (!apiPort) {
   process.exit(1);
 }
 
+const remoteSchemaUrls = process.env.REMOTE_SCHEMA_URLS?.split(',');
+if (!Array.isArray(remoteSchemaUrls)) {
+  console.error('process.env.REMOTE_SCHEMA_URLS not set');
+  process.exit(1);
+}
+
 export default {
   apiPort,
+  remoteSchemaUrls,
 };
