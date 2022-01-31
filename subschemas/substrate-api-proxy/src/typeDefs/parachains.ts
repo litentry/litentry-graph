8b12a1c1ea5ccd4dd19a53e2e148e1a803741521
Francisco Silva <franjs.francisco@gmail.com>
2022-01-31 17:47:46 +0100

    adding parachains typeDefs

---
+++ subschemas/substrate-api-proxy/src/typeDefs/parachains.ts
@@ -0,0 +1,42 @@
+export default /* GraphQL */ `
+  type ParachainsInfo {
+    parachainsCount: Int!
+    parathreadsCount: Int!
+    proposalsCount: Int!
+    leasePeriod: LeasePeriod!
+  }
+  type LeasePeriod {
+    currentLease: String!
+    totalPeriod: String!
+    progressPercent: Int!
+    remainder: String!
+  }
+  type Parachain {
+    id: String!
+    name: String
+    lease: Lease
+    lifecycle: String!
+    lastIncludedBlock: String!
+    lastBackedBlock: String!
+    homepage: String
+    validators: ValidatorsGroup
+    nonVoters: [AccountInfo!]
+  }
+  type Lease {
+    period: String
+    blockTime: String
+  }
+  type ValidatorsGroup {
+    groupIndex: String
+    validators: [AccountInfo!]
+  }
+  type AccountInfo {
+    address: String!
+    account: Account!
+  }
+  type Query {
+    parachainsInfo: ParachainsInfo!
+    parachains: [Parachain!]
+    parachain(id: String!): Parachain
+  }
+`;
