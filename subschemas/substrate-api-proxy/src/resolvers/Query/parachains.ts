b1c48122b07529a8538a494ac8d65bd3d4e33f15
Francisco Silva <franjs.francisco@gmail.com>
2022-02-02 12:28:22 +0100

    cleaning up parachains resolver

--- subschemas/substrate-api-proxy/src/resolvers/Query/parachains.ts
+++ subschemas/substrate-api-proxy/src/resolvers/Query/parachains.ts
@@ -1,15 +1,11 @@
-import type { Option, StorageKey } from '@polkadot/types';
 import type { ParaId } from '@polkadot/types/interfaces';
-import type { PolkadotRuntimeParachainsParasParaLifecycle } from '@polkadot/types/lookup';
 import type { Context } from '../../types';
 import type { ParachainsInfo } from '../../generated/resolvers-types';
 
-import { getLeasePeriod } from '../../services/parachainsService';
-
-type ParaIdEntries = [
-  StorageKey<[ParaId]>,
-  Option<PolkadotRuntimeParachainsParasParaLifecycle>,
-][];
+import {
+  getLeasePeriod,
+  getUpcomingParaIds,
+} from '../../services/parachainsService';
 
 export async function parachainsInfo(
   _: Record<string, never>,
@@ -18,10 +14,7 @@ export async function parachainsInfo(
 ): Promise<ParachainsInfo> {
   const parachainIds = await api.query.paras?.parachains?.<ParaId[]>();
   const proposals = await api.query.proposeParachain?.proposals?.entries();
-  const paraIdEntries = (await api.query.paras?.paraLifecycles?.entries()) as
-    | ParaIdEntries
-    | undefined;
-  const upcomingParaIds = extractUpcomingParaIds(paraIdEntries);
+  const upcomingParaIds = await getUpcomingParaIds(api);
   const leasePeriod = await getLeasePeriod(api);
 
   return {
@@ -30,28 +23,4 @@ export async function parachainsInfo(
     proposalsCount: proposals?.length ?? 0,
     leasePeriod,
   };
-}
-
-function extractUpcomingParaIds(entries: ParaIdEntries = []): ParaId[] {
-  return entries
-    .map(
-      ([
-        {
-          args: [paraId],
-        },
-        optValue,
-      ]): ParaId | null => {
-        const value = optValue.unwrapOr(null);
-
-        return value &&
-          (value.isParathread ||
-            value.isUpgradingParathread ||
-            value.isOffboardingParathread ||
-            value.isOnboarding)
-          ? paraId
-          : null;
-      },
-    )
-    .filter((paraId): paraId is ParaId => !!paraId)
-    .sort((a, b) => a.cmp(b));
 }
