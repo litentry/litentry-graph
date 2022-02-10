8809ba40458047562c0910f399441574c6d8917c
Ben Mason <ben@wattmason.co.uk>
2022-02-10 09:25:26 +0000

    update domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api-gateway.litentry.io)
+domains=(graph.litentry.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
