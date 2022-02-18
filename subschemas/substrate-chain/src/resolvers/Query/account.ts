7333f60fc4412821c4255ade8f487b7ab4a616f9
Francisco Silva <franjs.francisco@gmail.com>
2022-02-18 15:58:50 +0100

    refactoring account resolver

--- subschemas/substrate-chain/src/resolvers/Query/account.ts
+++ subschemas/substrate-chain/src/resolvers/Query/account.ts
@@ -1,37 +1,18 @@
-import type {RegistrationJudgement} from '../../generated/resolvers-types';
+import type {Account} from '../../generated/resolvers-types';
 import type {Context} from '../../types';
+import {AccountsService} from '../../services/accountsService';
 
-export const account = async (parent: {address?: string}, args: {address?: string}, context: Context) => {
-  const {api} = context;
+export const account = async (
+  parent: {address?: string},
+  args: {address?: string},
+  {api}: Context,
+): Promise<Account> => {
+  const accountsService = new AccountsService(api);
   const address = parent?.address || args?.address;
 
   if (!address) {
     throw new Error('address is required');
   }
 
-  const info = await api.derive.accounts.info(address);
-
-  const display = info.identity.displayParent
-    ? `${info.identity.displayParent}/${info.identity.display || info.identity.displayParent}`
-    : info.identity.display ?? address;
-
-  return {
-    address: address,
-    registration: {
-      ...info.identity,
-      judgements: info.identity.judgements.map<RegistrationJudgement>(([index, judgement]) => ({
-        index: index.toNumber(),
-        judgement: {
-          isErroneous: judgement.isErroneous,
-          isFeePaid: judgement.isFeePaid,
-          isKnownGood: judgement.isKnownGood,
-          isLowQuality: judgement.isLowQuality,
-          isOutOfDate: judgement.isOutOfDate,
-          isReasonable: judgement.isReasonable,
-          isUnknown: judgement.isUnknown,
-        },
-      })),
-    },
-    display: display.toUpperCase(),
-  };
+  return accountsService.getAccount(address);
 };
