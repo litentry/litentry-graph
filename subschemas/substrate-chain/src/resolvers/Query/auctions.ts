4ae8ed8fa8eec5630e0a302028016a049db7f38c
Ishrak Imam <ishrak@litentry.com>
2022-02-28 15:43:09 +0100

    Prettier formatting

--- subschemas/substrate-chain/src/resolvers/Query/auctions.ts
+++ subschemas/substrate-chain/src/resolvers/Query/auctions.ts
@@ -81,16 +81,17 @@ const getLatestAuction = (
     },
     raised: formatBalance(api, raised),
     raisedPercent,
-    winningBid: latestWinningBid ? {
-      isCrowdloan: Boolean(latestWinningBid.isCrowdloan),
-      firstSlot: formatNumber(latestWinningBid.firstSlot),
-      lastSlot: formatNumber(latestWinningBid.lastSlot),
-      blockNumber: lastWinners.blockNumber.toString(),
-      projectId: latestWinningBid.paraId.toString(),
-      projectName:
-        endpoints?.find((e) => e.paraId === latestWinningBid.paraId.toNumber())?.text?.toString() || '',
-      amount: formatBalance(api, lastWinners.total),
-    } : null,
+    winningBid: latestWinningBid
+      ? {
+          isCrowdloan: Boolean(latestWinningBid.isCrowdloan),
+          firstSlot: formatNumber(latestWinningBid.firstSlot),
+          lastSlot: formatNumber(latestWinningBid.lastSlot),
+          blockNumber: lastWinners.blockNumber.toString(),
+          projectId: latestWinningBid.paraId.toString(),
+          projectName: endpoints?.find((e) => e.paraId === latestWinningBid.paraId.toNumber())?.text?.toString() || '',
+          amount: formatBalance(api, lastWinners.total),
+        }
+      : null,
   };
 };
 
