ea74b05030fc3dd2a50f8566305596ab6abb192c
Mark Davison <github@iammark.co.uk>
2022-05-16 11:11:03 +0100

    Improve logging

--- graphql-server/src/substrateApi.ts
+++ graphql-server/src/substrateApi.ts
@@ -1,4 +1,4 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
+import {ApiPromise, WsProvider} from '@polkadot/api';
 
 export type SubstrateNetwork = 'kusama' | 'polkadot' | 'litmus' | 'khala';
 
@@ -21,19 +21,36 @@ export async function initSubstrateApi() {
   const apiPromises = [];
   const apis = {};
 
+  const reconnect = async (api, network) => {
+
+    if (api.isConnected) {
+      console.log(`${network} is connected`);
+      return;
+    }
+
+    console.log(`${network} is disconnected - reconnecting`);
+    await api.connect();
+
+    if (api.isConnected) {
+      console.log(`${network} has reconnected`);
+    } else {
+      console.error(`${network} failed to reconnect`);
+    }
+  }
+
   for (const [network, wsProvider] of Object.entries(providers)) {
     apiPromises.push(async () => {
       const api = await ApiPromise.create({provider: wsProvider});
       await api.isReady;
+
       api.once('error', async error => {
-        console.error(error);
-        if (!api.isConnected) {
-          console.log("Disconnected - reconnecting");
-          await api.connect();
-        }
+        console.error(`${network}: ${JSON.stringify(error)}`);
+        await reconnect(api, network);
       });
 
-      api.once('disconnected', () => api.connect())
+      api.once('disconnected', async () => {
+        await reconnect(api, network);
+      })
 
       apis[network] = api;
     })
